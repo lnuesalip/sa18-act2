d20fe51211bea8f928190ede849fbd2b564cb512
Learra Alip <lnuesalip@gmail.com>
2024-04-21 09:46:14 -0500

    Tested web app to make sure it worked

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,9 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render :index
+  end
+
   def show
     @product = Product.find(params[:id])
     render :show
