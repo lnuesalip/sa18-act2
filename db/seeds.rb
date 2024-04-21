e7a764186b2f3710a263349aaf0d7d28c4f7ba27
Learra Alip <lnuesalip@gmail.com>
2024-04-21 09:29:10 -0500

    Seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Product.create!(name: 'Apple', price: 0.99, description: 'A red fruit, very nutritious.')
+Product.create!(name: 'Banana', price: 1.99, description: 'A yellow fruit, great for shakes.')
+Product.create!(name: 'Orange', price: 1.29, description: 'An orange fruit, great for juice.')
